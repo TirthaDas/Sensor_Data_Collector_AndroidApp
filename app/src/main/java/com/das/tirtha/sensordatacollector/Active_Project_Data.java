1920276e92925498292f47fe24241649ba9e5f37
TirthaDas <tirthapratim02@gmail.com>
2019-08-16 23:42:32 +0100

    added new active project data class to have a common data type activeProject

--- app/src/main/java/com/das/tirtha/sensordatacollector/Active_Project_Data.java
+++ app/src/main/java/com/das/tirtha/sensordatacollector/Active_Project_Data.java
@@ -1,4 +1,37 @@
 package com.das.tirtha.sensordatacollector;
 
+import java.util.ArrayList;
+
 public class Active_Project_Data {
+    String projectTitle;
+    String Description;
+    String id;
+    String  sensorList;
+    String Userid;
+    public Active_Project_Data(String id,String projectTitle, String Description, String sensorList, String Userid)
+    {
+        this.projectTitle = projectTitle;
+        this.Description = Description;
+        this.sensorList=sensorList;
+        this.id = id;
+        this.Userid=Userid;
+
+    }
+    public String getProjectTitle()
+    {
+        return projectTitle;
+    }
+    public String getDescription()
+    {
+        return Description;
+    }
+    public String  getSensorList(){
+        return sensorList;
+    }
+    public String getId(){
+        return id;
+    }
+    public  String getUserid(){
+        return Userid;
+    }
 }
